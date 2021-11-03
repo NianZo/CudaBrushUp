1016e3045f1751fea285b48d82c595266e7b8eb1
NianZo <nicholas.angle@gmail.com>
2021-11-02 19:44:21 -0700

    Added serial vector summation code.

--- AddVectors/AddVectors.cpp
+++ AddVectors/AddVectors.cpp
@@ -7,15 +7,45 @@
 
 #include <array>
 #include <iostream>
+#include <chrono>
+#include <memory>
 
 constexpr uint64_t DATASIZE = 1000000;
 
+template <typename T>
+void SumArraySequential(T& target, const T& a, const T& b)
+{
+	for (uint64_t i = 0; i < DATASIZE; i++)
+	{
+		target[i] = a[i] + b[i];
+	}
+}
+
 int main()
 {
-	std::array<int, 1000000> a;
-	std::array<int, 1000000> b;
+	// Using a C-style array for now (with C++ smart-pointers)
+	std::unique_ptr a = std::make_unique<int[]>(DATASIZE);
+	std::unique_ptr b = std::make_unique<int[]>(DATASIZE);
 
-	//
-	a[0] = 1;
-	b[0] = 2;
+	auto startTime = std::chrono::steady_clock::now();
+
+	// Fill arrays with random data
+	srand(42);
+	for (uint64_t i = 0; i < DATASIZE; i++)
+	{
+		a[i] = rand();
+		b[i] = rand();
+	}
+
+	auto dataFilledTime = std::chrono::steady_clock::now();
+
+	// Sequentially sum arrays into target
+	std::unique_ptr sc = std::make_unique<int[]>(DATASIZE);
+	SumArraySequential(sc, a, b);
+
+	auto sequentialCalculationTime = std::chrono::steady_clock::now();
+
+	// Give output information
+	std::cout << "Time to fill arrays with data: " << static_cast<std::chrono::duration<double>>(dataFilledTime - startTime).count() << std::endl;
+	std::cout << "Time to sum arrays sequentially: " << static_cast<std::chrono::duration<double>>(sequentialCalculationTime - dataFilledTime).count() << std::endl;
 }
