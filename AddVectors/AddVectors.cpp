cf8a22da396b6908657f1567f78accd496ff099a
NianZo <nicholas.angle@gmail.com>
2021-11-02 18:21:46 -0700

    Initial commit, mostly set up workspace to begin to build and run code.

---
+++ AddVectors/AddVectors.cpp
@@ -0,0 +1,21 @@
+/*
+ * AddVectors.cpp
+ *
+ *  Created on: Nov 2, 2021
+ *      Author: nic
+ */
+
+#include <array>
+#include <iostream>
+
+constexpr uint64_t DATASIZE = 1000000;
+
+int main()
+{
+	std::array<int, 1000000> a;
+	std::array<int, 1000000> b;
+
+	//
+	a[0] = 1;
+	b[0] = 2;
+}
